f5266aa65bb47a099defe12954029f6939b7b142
Ahmed Abid <92954851+AhmedAb1d@users.noreply.github.com>
2023-04-12 04:43:26 +0100

    step2

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
 import Input from "@mui/material/Input";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
 import { useState } from "react";
-
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
 function App() {
   const [value, setValue] = useState("");
@@ -12,12 +16,146 @@ function App() {
       <h1>Blog Workshop</h1>
       <h2>By Inceptum</h2>
       <Input
-            id="standard-adornment-amount"
-            endAdornment={<SearchIcon/>}
-            onChange={(event) => setValue(event.target.value)}
-          />
+        id='standard-adornment-amount'
+        endAdornment={<SearchIcon />}
+        onChange={(event) => setValue(event.target.value)}
+      />
       <p>this is the output the search bar: {value} </p>
-
+      <div className='cards'>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component='img'
+              height='150'
+              image='/static/images/cards/contemplative-reptile.jpg'
+              alt='green iguana'
+            />
+            <CardContent>
+              <Typography gutterBottom variant='h5' component='div'>
+                Lizard
+              </Typography>
+              <Typography variant='body2' color='text.secondary'>
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component='img'
+              height='140'
+              image='/static/images/cards/contemplative-reptile.jpg'
+              alt='green iguana'
+            />
+            <CardContent>
+              <Typography gutterBottom variant='h5' component='div'>
+                Lizard
+              </Typography>
+              <Typography variant='body2' color='text.secondary'>
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component='img'
+              height='140'
+              image='/static/images/cards/contemplative-reptile.jpg'
+              alt='green iguana'
+            />
+            <CardContent>
+              <Typography gutterBottom variant='h5' component='div'>
+                Lizard
+              </Typography>
+              <Typography variant='body2' color='text.secondary'>
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component='img'
+              height='140'
+              image='/static/images/cards/contemplative-reptile.jpg'
+              alt='green iguana'
+            />
+            <CardContent>
+              <Typography gutterBottom variant='h5' component='div'>
+                Lizard
+              </Typography>
+              <Typography variant='body2' color='text.secondary'>
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component='img'
+              height='140'
+              image='/static/images/cards/contemplative-reptile.jpg'
+              alt='green iguana'
+            />
+            <CardContent>
+              <Typography gutterBottom variant='h5' component='div'>
+                Lizard
+              </Typography>
+              <Typography variant='body2' color='text.secondary'>
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component='img'
+              height='140'
+              image='/static/images/cards/contemplative-reptile.jpg'
+              alt='green iguana'
+            />
+            <CardContent>
+              <Typography gutterBottom variant='h5' component='div'>
+                Lizard
+              </Typography>
+              <Typography variant='body2' color='text.secondary'>
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component='img'
+              height='140'
+              image='/static/images/cards/contemplative-reptile.jpg'
+              alt='green iguana'
+            />
+            <CardContent>
+              <Typography gutterBottom variant='h5' component='div'>
+                Lizard
+              </Typography>
+              <Typography variant='body2' color='text.secondary'>
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </div>
     </div>
   );
 }
---
+++ Frontend/src/data.js
@@ -0,0 +1,46 @@
+const data = [
+    {
+        "id": 1,
+        "name": "Lizard",
+        "description": "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+        "image": "/static/images/cards/contemplative-reptile.jpg"
+    },
+    {
+        "id": 2,
+        "name": "Lion",
+        "description": "Lions are several species of African catrion, with over 6,000 species, ranging across all continents except Antarctica",
+        "image": "/static/images/cards/contemplative-reptile.jpg"
+    },
+    {
+        "id": 3,
+        "name": "Tiger",
+        "description": "Tigers are several species of African catrion, with over 6,000 species, ranging across all continents except Antarctica",
+        "image": "/static/images/cards/contemplative-reptile.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Elephant",
+        "description": "Elephants are several species of African catrion, with over 6,000 species, ranging across all continents except Antarctica",
+        "image": "/static/images/cards/contemplative-reptile.jpg"
+    },
+    {
+        "id": 5,
+        "name": "Giraffe",
+        "description": "Giraffes are several species of African catrion, with over 6,000 species, ranging across all continents except Antarctica",
+        "image": "/static/images/cards/contemplative-reptile.jpg"
+    },
+    {
+        "id": 6,
+        "name": "Zebra",
+        "description": "Zebras are several species of African catrion, with over 6,000 species, ranging across all continents except Antarctica",
+        "image": "/static/images/cards/contemplative-reptile.jpg"
+    },
+    {
+        "id": 7,
+        "name": "Hippo",
+        "description": "Hippos are several species of African catrion, with over 6,000 species, ranging across all continents except Antarctica",
+        "image": "/static/images/cards/contemplative-reptile.jpg"
+    }
+]
+
+export default data;
