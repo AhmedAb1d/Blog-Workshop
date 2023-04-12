4b5b0f75392130a54924cf630b4dc14802b5568b
Ahmed Abid <92954851+AhmedAb1d@users.noreply.github.com>
2023-04-12 04:16:20 +0100

    step1

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
-import logo from "./favicon.ico";
+import Input from "@mui/material/Input";
+import SearchIcon from '@mui/icons-material/Search';
+import { useState } from "react";
+
 
 function App() {
+  const [value, setValue] = useState("");
+
   return (
     <div className='App'>
-        <img src={logo} alt='logo' />
-        <h1>Blog Workshop</h1>
-        <h2>By Inceptum</h2>
+      <h1>Blog Workshop</h1>
+      <h2>By Inceptum</h2>
+      <Input
+            id="standard-adornment-amount"
+            endAdornment={<SearchIcon/>}
+            onChange={(event) => setValue(event.target.value)}
+          />
+      <p>this is the output the search bar: {value} </p>
+
     </div>
   );
 }
