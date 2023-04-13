b0d2a3763aff0948259bf2828a3bd6ed2b746d8c
Ahmed Abid <92954851+AhmedAb1d@users.noreply.github.com>
2023-04-13 20:32:10 +0100

    Create data.js

---
+++ Frontend/src/data.js
@@ -0,0 +1,46 @@
+const data = [
+    {
+        "id": 1,
+        "name": "Lizard",
+        "description": "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+        "image": "https://mcdn.wallpapersafari.com/medium/80/18/LPsFxU.jpg"
+    },
+    {
+        "id": 2,
+        "name": "Lion",
+        "description": "Lions are several species of African catrion, with over 6,000 species, ranging across all continents except Antarctica",
+        "image": "https://mcdn.wallpapersafari.com/medium/80/18/LPsFxU.jpg"
+    },
+    {
+        "id": 3,
+        "name": "Tiger",
+        "description": "Tigers are several species of African catrion, with over 6,000 species, ranging across all continents except Antarctica",
+        "image": "https://mcdn.wallpapersafari.com/medium/80/18/LPsFxU.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Elephant",
+        "description": "Elephants are several species of African catrion, with over 6,000 species, ranging across all continents except Antarctica",
+        "image": "https://mcdn.wallpapersafari.com/medium/80/18/LPsFxU.jpg"
+    },
+    {
+        "id": 5,
+        "name": "Giraffe",
+        "description": "Giraffes are several species of African catrion, with over 6,000 species, ranging across all continents except Antarctica",
+        "image": "https://mcdn.wallpapersafari.com/medium/80/18/LPsFxU.jpg"
+    },
+    {
+        "id": 6,
+        "name": "Zebra",
+        "description": "Zebras are several species of African catrion, with over 6,000 species, ranging across all continents except Antarctica",
+        "image": "https://mcdn.wallpapersafari.com/medium/80/18/LPsFxU.jpg"
+    },
+    {
+        "id": 7,
+        "name": "Hippo",
+        "description": "Hippos are several species of African catrion, with over 6,000 species, ranging across all continents except Antarctica",
+        "image": "https://mcdn.wallpapersafari.com/medium/80/18/LPsFxU.jpg"
+    }
+]
+
+export default data;
